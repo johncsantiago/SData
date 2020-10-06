7154a52ec8fa454dbad9a18fd4198305c1d9fb70
johncsantiago <61519162+johncsantiago@users.noreply.github.com>
2020-10-06 12:17:49 -0400

    Update Sasha Microarray Code.R

--- Sasha Microarray Code.R
+++ Sasha Microarray Code.R
@@ -1,28 +1,18 @@
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("pd.ragene.1.0.st.v1")
-
 library(oligo)
 library(affycoretools)
 library(limma)
-library(pd.hugene.2.0.st)
 library(pd.ragene.1.0.st.v1)
 setwd("/Users/johncsantiago/Documents/GitHub/SData/Joan_CEL_GCT_Files/ZIP050517EH11_(RaGene-1_0-st-v1)1hr/")
 
 
 gfs <- read.celfiles(list.files())
 ES <- rma(gfs)
-lowExprCutoff <- quantile(exprs(ES), 0.1) ## see Below (*)
+lowExprCutoff=log(50,base=2)
 pData(ES)$Cond <- factor(c("CtrlF", "CtrlM", "TreatF", "TreatM","CtrlF", "CtrlM", "TreatF", "TreatM","CtrlF", "CtrlM", "TreatF", "TreatM"))
 ES <- annotateEset(ES, "pd.ragene.1.0.st.v1")
 omit <- is.na(fData(ES)$SYMBOL)
 ES <- ES[!omit,]
 ES <- ES[apply(exprs(ES), 1, var) > 0.01, ]
-Means <- apply(exprs(ES), 1, mean)
-ES <- ES[order(Means, decreasing=TRUE), ]
-ES <- ES[!duplicated(fData(ES)$SYMBOL), ]
-ES <- ES[order(fData(ES)$PROBEID), ]
 maxvals <- apply(exprs(ES), 1, max)
 ES <- ES[maxvals > lowExprCutoff, ]
 
@@ -30,6 +20,7 @@ ES <- ES[maxvals > lowExprCutoff, ]
 design <- model.matrix(~ 0 + pData(ES)$Cond)
 colnames(design) <- levels(pData(ES)$Cond)
 fit <- lmFit(ES, design=design)
+
 fit2 <- contrasts.fit(fit, c(0,-1,0,1))
 fit2 <- eBayes(fit2)
 Result <- topTable(fit2, number=nrow(ES), sort.by="none")
@@ -39,91 +30,29 @@ names(Result) <- sub(".CEL$", "", names(Result))
 Result <- Result[order(Result$P.Value, decreasing=F), ]
 Result$GENENAME[Result$FDR<.05]
 
-setwd("/Users/johncsantiago/Documents/Elizabeth Files/")
-gfs <- read.celfiles(
-  "JT_071318_HUGENE_4A_ElDeiry_(HuGene-2_0-st).CEL", 
-  "JT_071318_HUGENE_4B_ElDeiry_(HuGene-2_0-st).CEL", 
-  "JT_071318_HUGENE_DMSOA_ElDeiry_(HuGene-2_0-st).CEL",
-  "JT_071318_HUGENE_DMSOB_ElDeiry_(HuGene-2_0-st).CEL")
+MaleEffect=Result
 
-## Do RMA normalization & get ExpressionSet object:
-ES <- rma(gfs)
+library(plot3D)
+groups <- colnames(Result)[11:22]
+pca <- prcomp(t(Result[,11:22]), scale.=TRUE) 
+gr=factor(groups)
 
-lowExprCutoff <- quantile(exprs(ES), 0.1) ## see Below (*)
+eigs <- pca$sdev^2
+ve=(eigs / sum(eigs))*100
+names(ve)=c("PC1","PC2","PC3")
+barplot(ve,ylim=c(0,20),cex.names =.8)
+dev.off()
+xcoords=pca$x[,1]
+ycoords=pca$x[,2]
+zcoords=pca$x[,3]
 
-## Add sample annotations:
-pData(ES)$Cond <- factor(c("Treat", "Treat", "Ctrl", "Ctrl"))
+text2D(x=xcoords,y=ycoords, group=gr, labels = gr, col=c("green3","red3","blue3","gold","green3","red3","blue3","gold","green3","red3","blue3","gold"),xlab="PC1",ylab="PC2")
 
-## Annotate probes with gene symbol, etc:
-ES <- annotateEset(ES, "pd.hugene.2.0.st")
+lines2D(x=xcoords[c(1,5,9)],y=ycoords[c(1,5,9)],col="green3",add=T,lty=1)
+lines2D(x=xcoords[c(2,6,10)],y=ycoords[c(2,6,10)],col="red3",add=T,lty=1)
+lines2D(x=xcoords[c(3,7,11)],y=ycoords[c(3,7,11)],col="blue3",add=T,lty=1)
+lines2D(x=xcoords[c(4,8,12)],y=ycoords[c(4,8,12)],col="gold",add=T,lty=1)
 
-## Remove probes which don't map to a gene:
-omit <- is.na(fData(ES)$SYMBOL)
-ES <- ES[!omit,]
+abline(h=0,lty=2)
+abline(v=0,lty=2)
 
-## Remove genes with negligible variance (0.01 is an arbitrary cutoff here,
-## nothing special):
-ES <- ES[apply(exprs(ES), 1, var) > 0.01, ]
-
-## Note that not all gene symbols are unique in ES. If we only want to
-## retain the probe for a gene that has the highest mean value, this is
-## how to do it:
-Means <- apply(exprs(ES), 1, mean)
-ES <- ES[order(Means, decreasing=TRUE), ]
-ES <- ES[!duplicated(fData(ES)$SYMBOL), ]
-ES <- ES[order(fData(ES)$PROBEID), ]
-
-## (*) As an example (not a recommendation), this would remove all probes
-## with maximum value (across 4 samples) less than the 10th percentile of
-## all expression values:
-maxvals <- apply(exprs(ES), 1, max)
-ES <- ES[maxvals > lowExprCutoff, ]
-
-maxvals <- apply(cpm(2^exprs(ES)), 1, max)
-maxvals=maxvals>10
-ES <- ES[maxvals, ]
-
-
-cpm(2^exprs(ES))
-##The log base 2 normalized expression table
-temp=2^exprs(ES)
-cpmtemp=cpm(temp)
-cutoff=7
-cpmtemp=cpmtemp[cpmtemp[,1]>cutoff,]
-cpmtemp=cpmtemp[cpmtemp[,2]>cutoff,]
-cpmtemp=cpmtemp[cpmtemp[,3]>cutoff,]
-cpmtemp=cpmtemp[cpmtemp[,4]>cutoff,]
-dim(cpmtemp)
-
-## These are the pieces to doo defferential expression analysis with limma:
-
-design <- model.matrix(~ 0 + pData(ES)$Cond)
-colnames(design) <- levels(pData(ES)$Cond)
-fit <- lmFit(ES, design=design)
-fit2 <- contrasts.fit(fit, c(-1, 1))
-fit2 <- eBayes(fit2)
-
-## Get results:
-Result <- topTable(fit2, number=nrow(ES), sort.by="none")
-Result <- cbind(Result, exprs(ES))
-names(Result) <- sub("adj.P.Val", "FDR", names(Result))
-names(Result) <- sub(".CEL$", "", names(Result))
-
-## Sort by significance. Up- and down-regulated genes will be
-## interspersed.
-Result <- Result[order(Result$P.Value, decreasing=F), ]
-
-## Write full table to tab-delimited file:
-write.table(Result, "limma.TreatVsCtrl.txt", sep="\t",
-            row.names=FALSE)
-
-## Note: this tab-delimited file can be opened in Excel, but it is a
-## well-known problem that some gene symbols (e.g., SEP1) will get
-## automatically (and irreversibly, if you save after opening) converted
-## to dates, unless you are careful about how you import the file. Human
-## gene symbols are currently being renamed to avoid this problem
-## (too hard to fix Excel's behavior, apparently), but many annotation
-## tools/files haven't caught up yet.
-## See for more info:
-## https://www.engadget.com/scientists-rename-genes-due-to-excel-151748790.html
-## https://www.biostars.org/p/211861/
